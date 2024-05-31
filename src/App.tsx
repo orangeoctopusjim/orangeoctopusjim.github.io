db1b61f9f663c6148e0186e3e52dd78239d9b425
Conor Byron <conorbyron@users.noreply.github.com>
2024-05-30 23:02:37 -0400

    Set up vite project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      Welcome to Orange Octopus Jim's wonderful web wetsperience.
+      <br />
+      <audio controls src="ladybug.m4a">
+        Play
+      </audio>
+    </>
+  )
+}
+
+export default App
