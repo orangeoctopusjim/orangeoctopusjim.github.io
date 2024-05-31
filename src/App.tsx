b75212665102caf89eeca0e0370b9f0593880a8e
Conor Byron <conorbyron@users.noreply.github.com>
2024-05-31 11:26:21 -0400

    Rename Play to Spin (test)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
       Welcome to Orange Octopus Jim's wonderful web wetsperience.
       <br />
       <audio controls src="ladybug.m4a">
-        Play
+        Spin
       </audio>
     </>
   )
