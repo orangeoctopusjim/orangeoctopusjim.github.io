5f85345946c69f785dac6651771434708916e741
Conor Byron <conorbyron@users.noreply.github.com>
2024-06-01 17:35:11 -0400

    Get some links in there

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BackgroundCanvas } from './Background'
 
 import titleImage from './assets/title.png'
 import jimImage from './assets/jim.png'
+import icon1 from './assets/icons/facebook.svg'
+import icon2 from './assets/icons/instagram.svg'
 
 function App() {
   return (
@@ -17,12 +19,40 @@ function App() {
             alt="Title"
           />
         </header>
-        <main className="flex justify-center items-center h-[75%]">
-          <img
-            className="max-h-full max-w-full object-contain"
-            src={jimImage}
-            alt="Jim"
-          />
+        <main className="flex justify-center items-center h-[75%] relative">
+          <div
+            className="relative"
+            style={{ aspectRatio: '16/9', width: '100%' }}
+          >
+            <img
+              className="absolute top-0 left-0 w-full h-full object-contain"
+              src={jimImage}
+              alt="Jim"
+            />
+            <a
+              href="#link1" // Replace with the desired link
+              className="absolute"
+              style={{
+                top: '65%',
+                left: '30%',
+                transform: 'translate(-50%, -50%)',
+              }}
+            >
+              <img src={icon1} alt="Icon 1" />
+            </a>
+            <a
+              href="#link2" // Replace with the desired link
+              className="absolute"
+              style={{
+                top: '70%',
+                left: '60%',
+                transform: 'translate(-50%, -50%)',
+              }}
+            >
+              <img src={icon2} alt="Icon 2" />
+            </a>
+            {/* Add more icons with appropriate links */}
+          </div>
         </main>
         <footer className="flex justify-center items-center text-white h-[10%]">
           Contact
