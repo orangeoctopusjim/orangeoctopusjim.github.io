23765396234752c459af84cfb4c90e8e23706af3
Conor Byron <conorbyron@users.noreply.github.com>
2024-06-01 16:55:29 -0400

    Aligned images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BackgroundCanvas } from './Background'
 
+import titleImage from './assets/title.png'
 import jimImage from './assets/jim.png'
 
 function App() {
@@ -8,8 +9,16 @@ function App() {
       <div className="w-full h-full absolute z-0 m-0">
         <BackgroundCanvas />
       </div>
-      <div className="w-full h-full flex absolute z-10 ">
-        <img className="" src={jimImage} />
+      <div className="w-full h-full flex flex-col absolute z-10 ">
+        <div className="flex justify-center items-center">
+          <img className="h-36" src={titleImage} />
+        </div>
+        <div className="flex justify-center items-center h-[600px]">
+          <img className="h-full w-auto" src={jimImage} />
+        </div>
+        <div className="flex justify-center items-center text-white">
+          Contact
+        </div>
       </div>
     </div>
   )
