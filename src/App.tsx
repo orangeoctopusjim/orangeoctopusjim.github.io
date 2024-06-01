35614c514d21f340639da1323158406845ab2cdb
Conor Byron <conorbyron@users.noreply.github.com>
2024-06-01 17:13:47 -0400

    Preserver image aspact ratio

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,24 @@ function App() {
       <div className="w-full h-full absolute z-0 m-0">
         <BackgroundCanvas />
       </div>
-      <div className="w-full h-full flex flex-col absolute z-10 ">
-        <div className="flex justify-center items-center">
-          <img className="h-36" src={titleImage} />
-        </div>
-        <div className="flex justify-center items-center h-[600px]">
-          <img className="h-full w-auto" src={jimImage} />
-        </div>
-        <div className="flex justify-center items-center text-white">
+      <div className="w-full h-full flex flex-col absolute z-10">
+        <header className="flex justify-center items-center h-[15%]">
+          <img
+            className="max-h-full max-w-full object-contain"
+            src={titleImage}
+            alt="Title"
+          />
+        </header>
+        <main className="flex justify-center items-center h-[75%]">
+          <img
+            className="max-h-full max-w-full object-contain"
+            src={jimImage}
+            alt="Jim"
+          />
+        </main>
+        <footer className="flex justify-center items-center text-white h-[10%]">
           Contact
-        </div>
+        </footer>
       </div>
     </div>
   )
